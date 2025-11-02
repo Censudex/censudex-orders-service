ec1eb1dffa5d125aa24c5cc44e156ced6004c13e
NicolasGO21 <trucelpro7@gmail.com>
2025-11-02 12:54:52 -0300

    feat: created a basic orders controller with few endpoints to test

---
+++ src/controllers/ordersController.js
@@ -0,0 +1,29 @@
+import * as orderService from '../services/order.service.js';
+
+export const createOrder = async (req, res, next) => {
+  try {
+    const order = await orderService.createOrder(req.body);
+    res.status(201).json(order);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getAllOrders = async (req, res, next) => {
+  try {
+    const orders = await orderService.getOrders();
+    res.json(orders);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getOrderById = async (req, res, next) => {
+  try {
+    const order = await orderService.getOrderById(req.params.id);
+    if (!order) return res.status(404).json({ message: 'Orden no encontrada' });
+    res.json(order);
+  } catch (err) {
+    next(err);
+  }
+};
