5ddfc556b1d7ae8bfaff22593b9a433e2d961760
NicolasGO21 <trucelpro7@gmail.com>
2025-11-15 22:44:18 -0300

    fix: fixed small error in order.proto

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,22 @@
+/**
+ * Archivo de exportación centralizado para los modelos de la aplicación
+ * Facilita la importación de modelos desde un único punto
+ * 
+ * @module models
+ * @description Centraliza las exportaciones de todos los modelos Sequelize
+ */
+
+// Importar modelo de Orden (Order)
+// Contiene la estructura y definición de una orden de compra
 import { Order } from './order.js';
+
+// Importar modelo de Items de Orden (OrderItem)
+// Contiene la estructura y definición de los artículos dentro de una orden
 import { OrderItem } from './orderItem.js';
 
+/**
+ * Exportar modelos para su uso en otros módulos
+ * Permite usar: import { Order, OrderItem } from './models/index.js'
+ * O simplemente: import { Order, OrderItem } from './models'
+ */
 export { Order, OrderItem };
